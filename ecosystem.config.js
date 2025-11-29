a80ca6465623d39cd091ccf4aaa988e03c062863
Toth Edina <tothedina2003@gmail.com>
2025-11-29 12:45:45 +0100

    eco modification 5

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,12 +4,11 @@ module.exports = {
       name: "backend",
       cwd: "/var/www/car-rental-platform",
       // mutat az aktuális JS entry-re a repo-n belül
-      script: "node",
-      args: "backend/src/backend/express.js",
+      script: "backend/src/backend/express.js",
       instances: 1,
       exec_mode: "cluster",
       watch: true,
-      env: {
+      env_production: {
         PORT: 3000,
         MONGODB_URI: "{{ mongodb_uri }}"
       }
